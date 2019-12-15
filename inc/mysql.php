93d711df819ea32bc45e07028424c48d7dfddfe1
yuesha <2661103378@qq.com>
2019-12-15 11:01:30 +0800

    获取聊天室成员功能基本完成

--- inc/mysql.php
+++ inc/mysql.php
@@ -16,6 +16,14 @@
         private $database = "chatRoom";
         // 最后一次错误信息
         public $errMsg = null;
+        // 最后一次执行的语句
+        public $lastQuery = null;
+        // 最后一次查询条件
+        public $where = null;
+        // 最后一次查询表名
+        public $table = null;
+        // 最后一次查询字段
+        public $field = '*';
 
         // 构造函数
         function __construct()
@@ -50,8 +58,54 @@
             if (!$this -> link_id) {
                 $this -> link();
             }
+            $this -> lastQuery = $sql;
             $res = mysqli_query($this -> link_id,$sql);
+            if (!$res) {
+                $this -> errMsg = "SQL语句：{$sql}，执行失败";
+                return false;
+            }
             return $res;
+        }
+
+        // 传入查询条件，返回对象
+        public function where($where)
+        {
+            $this -> where = $where;
+            return $this;
+        }
+
+        // 传入表名，返回对象
+        public function table($table)
+        {
+            $this -> table = $table;
+            return $this;
+        }
+
+        // 传入字段，返回对象
+        public function field($field = '*')
+        {
+            $this -> field = $field;
+            return $this;
+        }
+
+        // 执行查询，多条
+        public function select()
+        {
+            if ($this -> table == null || $this -> where == null || $this -> field == null || $this -> field == '') {
+                $this -> errMsg = "查询条件不完整";
+                return false;
+            }
+            $sql = "SELECT {$this -> field} FROM `{$this -> table}` WHERE {$this -> where}";
+            $res = $this -> execute($sql);
+            $rows = mysqli_fetch_all($res,MYSQLI_ASSOC);
+            return $rows;
+        }
+
+        // 获取最后一次执行的SQL语句
+        public function getLastQuery()
+        {
+            $sql = $this -> lastQuery == null ? '未执行过SQL语句' : $this -> lastQuery;
+            return $sql;
         }
     }
 ?>
