8f8fd36e4a38f09c5efd5417faf2e590e495ee94
yuesha <2661103378@qq.com>
2019-12-15 12:29:40 +0800

    页面显示加上昵称和时间

--- js/getInfo.js
+++ js/getInfo.js
@@ -6,11 +6,10 @@ function getInfo() {
             // console.log(res.data);
             // $('#').
             // for (var i = res.data.length - 1; i >= 0; i--) {
-            for (var i = 0; i < res.data.length; i++) {
+                for (var i = 0; i < res.data.length; i++) {
 
-                // var txt2=$();
-                // $("#infoBox").append(txt2);
-                var str = '<div class="news"><span>' + res.data[i].user_id + '(XXX)</span> <div class="text"> <p>' + res.data[i].content + '</p> </div> </div>';
+                    var time = formatTime(res.data[i].created_at);
+                    var str = '<div class="news"><span>' + res.data[i].nickname + '(' + time + ')</span> <div class="text"> <p>' + res.data[i].content + '</p> </div> </div>';
                 // console.log(str);
                 var obj = $(str);
                 $("#infoBox").append(obj);
@@ -27,4 +26,17 @@ function getInfo() {
     // 第二个参数： 需要发送到接口的数据
     // 第三个参数： 回调函数 -> 获取接口返回的数据
     // 第四个参数： 期待返回数据类型
+}
+
+function formatTime (time) {
+    // let unixtime = time;
+    // let unixTimestamp = new Date(unixtime * 1000);
+    // let Y = unixTimestamp.getFullYear();
+    // let M = ((unixTimestamp.getMonth() + 1) > 10 ? (unixTimestamp.getMonth() + 1) : '0' + (unixTimestamp.getMonth() + 1));
+    // let D = (unixTimestamp.getDate() > 10 ? unixTimestamp.getDate() : '0' + unixTimestamp.getDate());
+    // let toDay = Y + '-' + M + '-' + D;
+    // return toDay;
+    var unixTimestamp = new Date(time* 1000);
+    commonTime = unixTimestamp.toLocaleString();
+    return commonTime;
 }
